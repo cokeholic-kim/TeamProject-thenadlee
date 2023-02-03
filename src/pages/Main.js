10f3562e2084eba40d2e9abcbfb4be9f48f5abde
donghy <kimadonghyon99@gmail.com>
2023-02-03 16:51:09 +0900

    ver0.3

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,10 +1,12 @@
 import React from "react";
 import MainBottom from "./MainBottom";
+import MainTop from "./MainTop";
 
 function Main(){
 
     return(
         <div>
+            <MainTop/>
             <MainBottom/>
         </div>
     )
