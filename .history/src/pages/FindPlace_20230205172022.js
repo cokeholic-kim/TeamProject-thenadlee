0539d3ec5eec85182866ea599dc91af516c49117
star <kbmykbyeol@naver.com>
2023-02-05 20:43:03 +0900

    FindPlace.js and  FindPlace.scss changed

---
+++ .history/src/pages/FindPlace_20230205172022.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './FindPlace.scss' ;
+
+function FindPlace () {
+    return (
+        <div className='FindPlace'>
+            <div className='FindPlace_country'>
+                <div className='FindPlace_country_Jeju'>
+                    
+                </div>
+                <div className='FindPlace_country_Singapore'>
+
+                </div>
+                <div className='FindPlace_country_China'>
+
+                </div>
+                <div className='FindPlace_country_Canada'>
+
+                </div>
+
+            </div>
+        </div>
+    );
+};
+
+export default FindPlace;
--- .history/src/pages/Main_20230205203241.js
+++ .history/src/pages/Main_20230205203241.js
@@ -1,13 +1,11 @@
 import React from "react";
 import FindPlace from "./FindPlace";
 import MainBottom from "./MainBottom";
-import MainTop from "./MainTop";
 
 function Main(){
 
     return(
         <div>
-            <MainTop/>
             <MainBottom/>
             <FindPlace/>
         </div>
