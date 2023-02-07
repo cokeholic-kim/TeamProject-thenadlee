77c968018630ab6ce7663d1d228dfac9bf96abe2
star <kbmykbyeol@naver.com>
2023-02-07 17:55:28 +0900

    shit

---
+++ src/modules/add.js
@@ -0,0 +1,50 @@
+
+// 1.액션 타입
+const SET_PLUS = "SET_PLUS" ;
+const SET_DELETE = "SET_DELETE" ;
+
+// 2.액션 생성함수
+const setPlus = () => ({
+    type: SET_PLUS,
+    add: {
+        id: id++ ,
+        isAdded: false
+    }
+    
+})
+
+let id = 1
+export const setDelete = (id) => ({
+    type: SET_DELETE,
+    add: {
+        id ,
+        isAdded: true
+    }
+})
+
+// 3.초기값 생성
+export const initialState = {
+    adds: [],
+  };
+
+
+//  4. 리듀서 생성
+export function adds(state=initialState,action){
+    switch(action.type){
+        case SET_PLUS:
+            return{
+                ...state,
+                adds: state.adds.concat(action.add),
+            }
+        case SET_DELETE:
+            return{
+                ...state,
+                adds: state.adds.filter((add) => add.id !== action.id),
+            } ;
+        default:
+            return state;
+    }
+}
+
+
+export default adds;
