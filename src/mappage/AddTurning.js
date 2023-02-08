118c23195459ffb25a35c10fb96e20f19d14ae69
donghyeonkim <kimadonghyon99@gmail.com>
2023-02-08 18:38:31 +0900

    stars commit

--- src/mappage/AddTurning.js
+++ src/mappage/AddTurning.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { FaInfoCircle } from 'react-icons/fa';
 import { setDelete } from '../modules/add';
+import './AddTurning.scss' ;
 
 // 데이터 카드(li)가 LeftControlbar로 넘어가기 위한 컴포넌트
 
@@ -13,16 +14,16 @@ const AddTurning = ({nation,lat,lng,spotname}) => {
         dispatch(setDelete(spotname))
     }
     return (
-            <li>
-                <div>
-                    <img src="https://www.myro.co.kr/getSpotImage/amsterdam?no=1021"></img>
+            <li className="AddTurning_contents_li">
+                <div className="AddTurning_contents_img">
+                    <img className='AddTurning_contents_img_i' src="https://www.myro.co.kr/getSpotImage/amsterdam?no=1021"></img>
                 </div>
-                <div>
-                    <div>
+                <div className="AddTurning_contents_p">
+                    <div className='AddTurning_contents_p_left'>
                         <p>{nation}</p>
                         <p><span>{spotname}</span></p>
                     </div>
-                    <div>
+                    <div className='AddTurning_contents_p_right'>
                         <div><FaInfoCircle/></div>
                         <div><span onClick={onclick}>X</span></div>
                     </div>
--- src/mappage/LeftControlbar.js
+++ src/mappage/LeftControlbar.js
@@ -17,7 +17,7 @@ const LeftControlbar = ({place}) => {
                 </div>
                 <div className='planeticketing'>
                     <div id="Flightbutton">
-                        <button>
+                        <button className="Flightbutton">
                             <MdAirplaneTicket/>
                             <img style={{width:"80px"}} src="../imgs/kyowon.png" alt="placephoto" loading='lazy'></img>
                         </button>
@@ -26,38 +26,37 @@ const LeftControlbar = ({place}) => {
                 <div className='selectList'>
                     선택목록
                 </div>
-                    <div className="center" style={{margin:"16px 0",width:"100%" }}>
-                        <ul id="tabs" className="tabs tabs-fixed-width">
-                            <li className="tab">
-                                <a id="selecteSpotsTab" style={{textDecoration:"none !important"}}>장 소</a>
+                <div className="center" style={{margin:"16px 0",width:"100%" }}>
+                    <ul id="tabs" className="tabs tabs-fixed-width">
+                        <li className="tab">
+                            <a id="selecteSpotsTab" style={{textDecoration:"none !important"}}>장 소</a>
+                        </li>
+                    </ul>
+                </div>
+                <div className="left_bottom_ul">
+                    <div className="center" style={{margin:"8px 0"}}>
+                        <span id="seletedSpotsCount">0</span>
+                        <span id="totalTimeArea"><span>(총</span><span id="sumOfSpotStayingH">0</span><span data-langnum="20">시간</span>)</span>
+                    </div>
+                    <div className="center2" style={{display:"flex", justifyContent: "center" , alignItems: "center", width: "100%" , padding: "8px 0"}}>
+                        <button className="Clearbtn" onClick={(e)=>{console.log(e)}}>
+                            <h6>장소전체삭제</h6>
+                        </button>
+                    </div>
+                    <ul className="ul-style" id="cart">
+                        {/* 들어갈위치 */}
+                        { places.length != 0 ? places.map((d,index)=><AddTurning key={index} lat={d.lat} lng={d.lng} nation={d.nation} spotname={d.spotname}/>): 
+                            <li id="cartList" className="center">
+                                <hs>
+                                    <span data-langnum="27">가고 싶은 장소들을 검색하여 추가해주세요.</span><br/>
+                                    <span data-langnum="28">설정하신 일자별 여행시간 내에서 </span><br/>
+                                    <span data-langnum="29">하루 평균 최대 8개의 장소까지 선택 가능합니다.</span><br/><br/>
+                                    <AiOutlinePlus/>
+                                </hs>
                             </li>
-                        </ul>
-                    </div>
-                    <div>
-                        <div className="center" style={{margin:"8px 0"}}>
-                            <span id="seletedSpotsCount">0</span>
-                            <span id="totalTimeArea"><span>(총</span><span id="sumOfSpotStayingH">0</span><span data-langnum="20">시간</span>)</span>
-                        </div>
-                        <div style={{display:"flex", justifyContent: "center" , alignItems: "center", width: "100%" , padding: "8px 0"}}>
-                            <button className="Clearbtn" onClick={(e)=>{console.log(e)}}>
-                                <h6>장소전체삭제</h6>
-                            </button>
-                        </div>
-                        <ul className="ul-style" id="cart">
-                            {/* 들어갈위치 */}
-                            { places.length != 0 ? places.map((d,index)=><AddTurning key={index} lat={d.lat} lng={d.lng} nation={d.nation} spotname={d.spotname}/>): 
-                                <li id="cartList" className="center">
-                                    <hs>
-                                        <span data-langnum="27">가고 싶은 장소들을 검색하여 추가해주세요.</span><br/>
-                                        <span data-langnum="28">설정하신 일자별 여행시간 내에서 </span><br/>
-                                        <span data-langnum="29">하루 평균 최대 8개의 장소까지 선택 가능합니다.</span><br/><br/>
-                                        <AiOutlinePlus/>
-                                    </hs>
-                                </li>
-                            }
-                        
-                        </ul>
-                    </div>
+                        }
+                    </ul>
+                </div>
                 </div>
             </div>            
     );
